0f3c6e7312be7c38f03a32baee601df4201a85c3
tyh1125 <1400738177@qq.com>
2019-01-27 19:52:00 +0800

    主页布局

---
+++ web-new/src/views/main-content.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="wrap-main"></main>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+    .wrap-main{
+        padding:15px;
+    }
+</style>
+
--- web-new/src/views/main.vue
+++ web-new/src/views/main.vue
@@ -1,21 +1,32 @@
 <template>
   <div>
-    main
-    <router-link :to="{name: 'types'}">types</router-link>
-    <router-link :to="{name: 'dishes'}">dishes</router-link>
-    <router-link :to="{name: 'tables'}">tables</router-link>
-    <router-link :to="{name: 'info'}">info</router-link>
-    <router-link :to="{name: 'income'}">income</router-link>
-    <router-link :to="{name: 'records'}">records</router-link>
+    <main-navbar />
+    <main-sidebar />
+    <div class="wrap-content">
+      <main-content />
+    </div>
     <router-view></router-view>
   </div>
 </template>
 <script>
-
+import MainNavbar from './main-navbar'
+import MainSidebar from './main-sidebar'
+import MainContent from './main-content'
 export default {
-  name: 'main-index'
+  name: 'main-index',
+  components: {
+    MainNavbar,
+    MainSidebar,
+    MainContent
+  }
 }
 </script>
 
 <style>
+  .wrap-content{
+      padding-top: 50px;
+      margin-left: 230px;
+      min-height: 100%;
+      background: #f1f4f5;
+  }
 </style>
