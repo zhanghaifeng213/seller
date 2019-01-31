7d4b8402bab5f602ed1e30ddadca317e10e5bdc6
RocketPeng <372447858@qq.com>
2019-01-31 23:20:35 +0800

    用户管理完成功能(未完成分页)

--- web-new/src/fetch/user.js
+++ web-new/src/fetch/user.js
@@ -14,3 +14,27 @@ export const info = () => {
     method: 'get'
   })
 }
+
+export const inquire = (data) => {
+  return http({
+    url: http.adornUrl('/user/inquire'),
+    method: 'get',
+    data: data
+  })
+}
+
+export const userReg = (data) => {
+  return http({
+    url: http.adornUrl('user/reg'),
+    method: 'post',
+    data: data
+  })
+}
+
+export const userUpdate = (data) => {
+  return http({
+    url: http.adornUrl('user/update'),
+    method: 'post',
+    data: data
+  })
+}
