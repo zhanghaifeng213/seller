67f1e5074ccf368579727ae54c8900cbc2a6e6c0
敖晓强 <aoxiaoqiang@163.com>
2019-02-17 14:51:53 +0800

    feat: 展示优化

--- web-new/src/views/main-content.vue
+++ web-new/src/views/main-content.vue
@@ -1,9 +1,9 @@
 <template>
     <main class="wrap-main">
         <div>
-            <keep-alive>
-                <router-view />
-            </keep-alive>
+            <!-- <keep-alive><router-view /></keep-alive> -->
+            <!-- 去掉了keep-alive, 保证每次且切换到对应模块数据能够刷新 -->
+            <router-view />
         </div>
     </main>
 </template>
