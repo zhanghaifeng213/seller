05a0df93e7c8d70bec1ac871e676616c077c8c6b
zhanghf <751434992@qq.com>
2019-03-06 10:24:33 +0800

    0306

--- mobile/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ROOT: '"http://116.62.147.91:3030"',
-  SOCKETIO: '"http://116.62.147.91:3030"'
-}
--- server/Models/order.js
+++
@@ -1,6 +0,0 @@
-const { db } = require('../Schema/config')
-
-const OrderSchema = require('../Schema/table')
-const Order = db.model("tables", OrderSchema)
-
-module.exports = Order
